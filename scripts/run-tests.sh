248b0c290381fdc71cc00bb8bcb1478ec0362cb2
Paulo Matos <paulo@matos-sorge.com>
2014-08-18 20:07:53 +0100

    * scripts/run-tests.sh: Check for exit status.

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,17 +2,27 @@
 
 OPENSMT=$1
 TESTDIR=$2
+DIR=$(dirname "${BASH_SOURCE[0]}")
 
-for test in `find . -name '*.smt2'`; do
+for test in `find $TESTDIR -name '*.smt2'`; do
     echo -n $test " "
     
-    RES=$($1 $test 2> /dev/null)
+    RES=$($DIR/timeout.sh -t 120 $OPENSMT $test 2> /dev/null)
+    STATUS="$?"
     regex='\([[:space:]]*set-info[[:space:]]+:status[[:space:]]+([[:alpha:]]+)[[:space:]]*\)'
     
     if ! grep -q ':status' $test; then
 	echo SKIP
 	continue
     fi
+    if [ "$STATUS" == "143" ]; then
+	echo TIMEOUT
+	continue
+    elif [ "$STATUS" != "0" ]; then
+	echo EXIT_FAIL
+	continue
+    fi
+
     line=`cat $test | grep ':status'`
     [[  $line =~ $regex ]] || (echo REGEXP FAIL && continue)
     EXPECTED=${BASH_REMATCH[1]}
